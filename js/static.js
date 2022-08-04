5201cf3f40c1e14b913748bb3cde4036886480df
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-08-04 15:49:18 +0300

    in the process;)

--- js/static.js
+++ js/static.js
@@ -1,10 +1,11 @@
+// настройки слайдера
 $(document).ready(function() {
     $(".specialists__slider").slick({
         arrows: true,
         dots: true,
         slidesToShow: 4,
-        // speed: 1000,
-        // autoplay: true,
+        speed: 1000,
+        autoplay: true,
         slidesToScroll: 4,
         responsive: [{
                 breakpoint: 1367,
@@ -14,7 +15,7 @@ $(document).ready(function() {
                 },
             },
             {
-                breakpoint: 747,
+                breakpoint: 800,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
@@ -22,4 +23,18 @@ $(document).ready(function() {
             },
         ],
     });
+});
+// открытие и заркытие мобильного меню
+const burgerNode = document.querySelector(".header__burger");
+const dropdownMenuNode = document.querySelector(".m-menu");
+const contentNode = document.querySelector(".wrapper-content");
+const closeMenuNode = document.querySelector(".m-menu__icon-close");
+burgerNode.addEventListener("click", function() {
+    dropdownMenuNode.classList.add("m-menu--js-dropdown");
+    contentNode.classList.add("wrapper-content--js-close");
+});
+
+closeMenuNode.addEventListener("click", function() {
+    dropdownMenuNode.classList.remove("m-menu--js-dropdown");
+    contentNode.classList.remove("wrapper-content--js-close");
 });
